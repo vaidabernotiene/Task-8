52063d3a5f1e96334df987bf80fea32e877e7bd3
vaidabernotiene <vaidabernotiene@yahoo.com>
2023-02-20 13:36:42 +0200

    4 functions with prototype (+ ,- ,* ,/)

--- script.js
+++ script.js
@@ -6,9 +6,27 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+a = 25;
+b = 10;
 
 function Calculator() {
-  
+  this.rezult;
 }
+Calculator.prototype.getSum = function (a, b) {
+  console.log(`Sum: ${(this.rezult = a + b)}.`);
+};
+Calculator.prototype.getSub = function (a, b) {
+  console.log(`Substraction: ${(this.rezult = a - b)}.`);
+};
+Calculator.prototype.getMult = function (a, b) {
+  console.log(`Multiplication: ${(this.rezult = a * b)}.`);
+};
+Calculator.prototype.getDiv = function (a, b) {
+  console.log(`Division: ${(this.rezult = a / b)}.`);
+};
 
 const calculator = new Calculator();
+calculator.getSum(a, b);
+calculator.getSub(a, b);
+calculator.getMult(a, b);
+calculator.getDiv(a, b);
